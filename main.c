cae1b310b329d52575e932294b400d2730c9e6bd
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2022-05-09 01:05:52 +0100

    Update main.c

--- main.c
+++ main.c
@@ -197,7 +197,7 @@ GLFWwindow* window;
 uint winw = 1024;
 uint winh = 768;
 double t = 0;   // time
-f32 dt = 0;  // delta time
+f32 dt = 0;     // delta time
 double fc = 0;  // frame count
 double lfct = 0;// last frame count time
 f32 aspect;
